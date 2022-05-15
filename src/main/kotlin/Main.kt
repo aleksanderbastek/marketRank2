50959d42d34913cc8a60423a6c39142aadd095bf
aleksanderbastek <62114300+aleksanderbastek@users.noreply.github.com>
2022-05-15 19:52:24 +0200

    Add SortedSpreadOfMarket Data Class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,10 @@ fun getSpread(ticker_id: String): SpreadOfMarket {
     val orderBook = RestTemplate().getForObject("https://public.kanga.exchange/api/market/orderbook/$ticker_id", OrderBook::class.java)
     val bids: List<Double> = orderBook.bids.map {it[0]}
     val asks: List<Double> = orderBook.asks.map {it[0]}
+
     if (bids.isEmpty() || asks.isEmpty())
         return SpreadOfMarket(ticker_id,null)
+
     val minBid = bids.maxOf { it }
     val maxAsk = asks.minOf { it }
     val substrBidAsk = maxAsk - minBid
@@ -14,11 +16,17 @@ fun getSpread(ticker_id: String): SpreadOfMarket {
     return SpreadOfMarket(ticker_id,(substrBidAsk / (0.5 * sumBidAsk))*100)
 }
 
-fun getSpreadsOfAllMarkets(): List<SpreadOfMarket> {
+fun getSpreadsOfAllMarkets(): SortedSpreadOfMarket {
     val markets: List<String> = getTickerIds()
     val spreadsOfAllMarkets: List<SpreadOfMarket> =  markets.map { getSpread(it) }
 
-    return spreadsOfAllMarkets
+    val lessOrEqual2: List<SpreadOfMarket> = spreadsOfAllMarkets.filter { it.spread != null &&  it.spread <= 2.0}.sortedBy { it.ticker_id }
+    val moreThan2: List<SpreadOfMarket> = spreadsOfAllMarkets.filter { it.spread != null &&  it.spread > 2.0}.sortedBy { it.ticker_id }
+    val useless: List<SpreadOfMarket> = spreadsOfAllMarkets.filter { it.spread == null}.sortedBy { it.ticker_id }
+
+    val sortedSpreadOfMarket: SortedSpreadOfMarket = SortedSpreadOfMarket(lessOrEqual2, moreThan2, useless)
+
+    return sortedSpreadOfMarket
 }
 
 fun getTickerIds(): List<String> {
@@ -28,7 +36,7 @@ fun getTickerIds(): List<String> {
 }
 
 fun main(args: Array<String>) {
-    println(getSpreadsOfAllMarkets())
+    println(getSpreadsOfAllMarkets().useless)
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
@@ -38,3 +46,4 @@ fun main(args: Array<String>) {
 data class Market(val ticker_id: String)
 data class OrderBook(val bids: List<List<Double>>, val asks: List<List<Double>>)
 data class SpreadOfMarket(val ticker_id: String, val spread: Double?)
+data class SortedSpreadOfMarket(val lessOrEqual2: List<SpreadOfMarket>, val moreThan2: List<SpreadOfMarket>,val useless: List<SpreadOfMarket>,)
