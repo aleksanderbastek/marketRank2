153ed3d99cf9bc7b4a99426d89abd2f75009abef
aleksanderbastek <62114300+aleksanderbastek@users.noreply.github.com>
2022-05-20 10:55:14 +0200

    Refactor code, add marketRank2Models and SpreadUtilities packages

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
-import SpreadUtilities.SortedSpreadOfMarket
-import SpreadUtilities.getSpreadsOfAllMarkets
-import SpreadUtilities.writeMarketsToFile
-
+import org.marketRank2Utilities.SpreadUtilities
+import org.marketRank2Models.SortedSpreadOfMarket as SortedSpreadOfMarket
 fun main(args: Array<String>) {
+    val spreadUtilities: SpreadUtilities = SpreadUtilities()
     println("Wait for fetching data")
-    val sortedSpreadOfMarket: SortedSpreadOfMarket = getSpreadsOfAllMarkets()
+    val sortedSpreadOfMarket: SortedSpreadOfMarket = spreadUtilities.getSpreadsOfAllMarkets()
     println("Wait for writing data into a file")
-    writeMarketsToFile(sortedSpreadOfMarket)
+    spreadUtilities.writeMarketsToFile(sortedSpreadOfMarket)
 }
 
+data class SortedSpreadOfMarket(val lessOrEqual2: List<SortedSpreadOfMarket>, val moreThan2: List<SortedSpreadOfMarket>, val useless: List<SortedSpreadOfMarket>)
---
+++ src/main/kotlin/org/marketRank2Models/Market.kt
@@ -0,0 +1,6 @@
+package org.marketRank2Models
+
+data class Market(val ticker_id: String)
+data class OrderBook(val bids: List<List<Double>>, val asks: List<List<Double>>)
+data class SpreadOfMarket(val ticker_id: String, val spread: Double?)
+data class SortedSpreadOfMarket(val lessOrEqual2: List<SpreadOfMarket>, val moreThan2: List<SpreadOfMarket>,val useless: List<SpreadOfMarket>)
